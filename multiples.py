ddb131625c90d272e305bce4b7a7316f569c75c7
Ryan Armes <ryancarmes@gmail.com>
2018-03-07 15:33:00 -0500

    first commit

---
+++ multiples.py
@@ -0,0 +1,12 @@
+i = 1
+
+for i in range(1, 1001): #for a variable (i) count in a range 1 to 1001 by increments of 1
+    print i #output 1 to 1000 printed in subsequent/sequential lines
+
+for i in range(5, 1000001, 5): #for a variable (i) count in a range 5 to 1,000,001 by increments of 5
+    print i #output 5 to 1,000,000 by increments of 5 in subsquent/sequential lines
+
+a = [1, 2, 5, 10, 255, 3]
+print sum(a) #prints the sum of the list/array (a), since all data types are numbers
+
+print sum(a) / len(a) #prints the average of the list/array (a), since all data types are numbers. Average = sum divided by the number of values
